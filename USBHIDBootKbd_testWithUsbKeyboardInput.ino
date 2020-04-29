9da95e746d26096f72b94011706852ed1feec547
Stéphane Adam Garnier <seedsodesign@gmail.com>
2020-04-29 17:34:16 +0200

    Create USBHIDBootKbd_testWithUsbKeyboardInput.ino

---
+++ USBHIDBootKbd_testWithUsbKeyboardInput.ino
@@ -0,0 +1,146 @@
+/*
+    Testing Usb Keyboard Input on Sparkfun USB Host Shield rev 1.3 ( 09947 ) & USB Host Librayr V2. from Circuits@Home
+
+    -> works fine 'as is' ;P
+    
+    last update on: 29/04/2020
+    by StephaneAG - 2020
+    
+*/
+
+#include <hidboot.h>
+#include <usbhub.h>
+
+// Satisfy the IDE, which needs to see the include statment in the ino too.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#endif
+#include <SPI.h>
+
+// Needs to make Sparkfun's USB Host Shield rev1.3 (DEV-09947) work with the USB Host Shield library v2.0 from Circuits@Home ( from Sparkfun-related products FAQ page )
+#define MAX_RESET 7 // MAX chip pin 12
+#define MAX_GPX   8 // MAX chip pin 17
+
+class KbdRptParser : public KeyboardReportParser
+{
+    void PrintKey(uint8_t mod, uint8_t key);
+
+  protected:
+    void OnControlKeysChanged(uint8_t before, uint8_t after);
+
+    void OnKeyDown	(uint8_t mod, uint8_t key);
+    void OnKeyUp	(uint8_t mod, uint8_t key);
+    void OnKeyPressed(uint8_t key);
+};
+
+void KbdRptParser::PrintKey(uint8_t m, uint8_t key)
+{
+  MODIFIERKEYS mod;
+  *((uint8_t*)&mod) = m;
+  Serial.print((mod.bmLeftCtrl   == 1) ? "C" : " ");
+  Serial.print((mod.bmLeftShift  == 1) ? "S" : " ");
+  Serial.print((mod.bmLeftAlt    == 1) ? "A" : " ");
+  Serial.print((mod.bmLeftGUI    == 1) ? "G" : " ");
+
+  Serial.print(" >");
+  PrintHex<uint8_t>(key, 0x80);
+  Serial.print("< ");
+
+  Serial.print((mod.bmRightCtrl   == 1) ? "C" : " ");
+  Serial.print((mod.bmRightShift  == 1) ? "S" : " ");
+  Serial.print((mod.bmRightAlt    == 1) ? "A" : " ");
+  Serial.println((mod.bmRightGUI    == 1) ? "G" : " ");
+};
+
+void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)
+{
+  Serial.print("DN ");
+  PrintKey(mod, key);
+  uint8_t c = OemToAscii(mod, key);
+
+  if (c)
+    OnKeyPressed(c);
+}
+
+void KbdRptParser::OnControlKeysChanged(uint8_t before, uint8_t after) {
+
+  MODIFIERKEYS beforeMod;
+  *((uint8_t*)&beforeMod) = before;
+
+  MODIFIERKEYS afterMod;
+  *((uint8_t*)&afterMod) = after;
+
+  if (beforeMod.bmLeftCtrl != afterMod.bmLeftCtrl) {
+    Serial.println("LeftCtrl changed");
+  }
+  if (beforeMod.bmLeftShift != afterMod.bmLeftShift) {
+    Serial.println("LeftShift changed");
+  }
+  if (beforeMod.bmLeftAlt != afterMod.bmLeftAlt) {
+    Serial.println("LeftAlt changed");
+  }
+  if (beforeMod.bmLeftGUI != afterMod.bmLeftGUI) {
+    Serial.println("LeftGUI changed");
+  }
+
+  if (beforeMod.bmRightCtrl != afterMod.bmRightCtrl) {
+    Serial.println("RightCtrl changed");
+  }
+  if (beforeMod.bmRightShift != afterMod.bmRightShift) {
+    Serial.println("RightShift changed");
+  }
+  if (beforeMod.bmRightAlt != afterMod.bmRightAlt) {
+    Serial.println("RightAlt changed");
+  }
+  if (beforeMod.bmRightGUI != afterMod.bmRightGUI) {
+    Serial.println("RightGUI changed");
+  }
+
+}
+
+void KbdRptParser::OnKeyUp(uint8_t mod, uint8_t key)
+{
+  Serial.print("UP ");
+  PrintKey(mod, key);
+}
+
+void KbdRptParser::OnKeyPressed(uint8_t key)
+{
+  Serial.print("ASCII: ");
+  Serial.println((char)key);
+};
+
+USB     Usb;
+//USBHub     Hub(&Usb);
+HIDBoot<USB_HID_PROTOCOL_KEYBOARD>    HidKeyboard(&Usb);
+
+KbdRptParser Prs;
+
+void setup()
+{
+  Serial.begin( 115200 );
+#if !defined(__MIPSEL__)
+  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
+  Serial.println("Start");
+
+  // Needs to make Sparkfun's USB Host Shield rev1.3 (DEV-09947) work with the USB Host Shield library v2.0 from Circuits@Home ( from Sparkfun-related products FAQ page )
+  pinMode(MAX_GPX, INPUT);
+  pinMode(MAX_RESET, OUTPUT);
+  digitalWrite(MAX_RESET, LOW);
+  delay(20); // wait 20ms
+  digitalWrite(MAX_RESET, HIGH);
+  delay(20); // wait 20ms
+
+  if (Usb.Init() == -1)
+    Serial.println("OSC did not start.");
+
+  delay( 200 );
+
+  HidKeyboard.SetReportParser(0, &Prs);
+}
+
+void loop()
+{
+  Usb.Task();
+}
